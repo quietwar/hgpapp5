8c9463f1879b988a28d1b4a15f4b6a51a967d32a
Prof. Hodari Touré <hodari@hiddengeniusproject.org>
2017-05-31 16:18:14 -0700

    workout6

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @message = current_user.messages.build(message_params)
+    @message.room = current_room
+    
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to user_exercises_path(current_user) }
+        format.js { ActionCable.server.broadcast "messages_room_#{current_room.id}", render(partial: 'shared/message', object: @message) }
+      end
+    end
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: "dashboards#index"
+  
+  resources :users do
+    resources :exercises
+  end
+  
+  resources :dashboards, only: [:index] do
+    collection do
+      post :search, to: 'dashboards#search'
+    end
+  end
+  
+  resources :friendships, only: [:show, :create, :destroy]
+  resources :messages, only: [:create]
+  
+  mount ActionCable.server => '/cable'
+  
+end
